7eccaebf6ca5c4942257697863345bbbf99ae2d2
mjahaanzaib <muhammadjahaannzaib@gmail.com>
2024-03-12 02:44:03 +0500

    Token updated

--- src/main/java/org/acme/main/MainResource.java
+++ src/main/java/org/acme/main/MainResource.java
@@ -7,7 +7,7 @@ import org.acme.service.ApacheReposService;
 public class MainResource {
 
 	private static final Logger logger = Logger.getLogger("MainResource");
-	private static final String myToken = "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";
+	private static final String myToken = "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";
 	private static final String gitHubAccessToken = System.getProperty("githubToken");
 
 	public static void main(String[] args) {
@@ -16,7 +16,6 @@ public class MainResource {
 			ApacheReposService apacheReposService = new ApacheReposService(gitHubAccessToken);
 			logger.info(
 					"========= Kindly wait for a while to gather the required information from apache repositories =========");
-			// apacheReposService = new ApacheReposService();
 			apacheReposService.fnSaveContributorInfo();
 			logger.info("========= Required information from apache repositories has been stored in H2 =========");
 
@@ -27,13 +26,13 @@ public class MainResource {
 
 		else {
 
-			logger.info("========= You can provide your access token by CLI argument, like -DgithubToken=<YOUR_ACCESS_TOKEN> =========");
+			logger.info(
+					"========= You can provide your access token by CLI argument, like -DgithubToken=<YOUR_ACCESS_TOKEN> =========");
 			logger.info("========= You are currently using developer token =========");
 
 			ApacheReposService apacheReposService = new ApacheReposService(myToken);
 			logger.info(
 					"========= Kindly wait for a while to gather the required information from apache repositories =========");
-			// apacheReposService = new ApacheReposService();
 			apacheReposService.fnSaveContributorInfo();
 			logger.info("========= Required information from apache repositories has been stored in H2 =========");
 
